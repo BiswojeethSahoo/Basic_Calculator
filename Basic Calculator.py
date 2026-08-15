9efecea07da7eb4d3fec627b79280ab82584ee03
BiswojeethSahoo <biswojeethsahoo9392@gmail.com>
2026-08-15 21:55:20 +0530

    The code of the Calculator

---
+++ Basic Calculator.py
@@ -0,0 +1,18 @@
+a=float(input("Enter a number: "))
+b=float(input("Enter a number: "))
+
+print("The sum of the two numbers is:", a+b)
+print("The difference of the two numbers is:", a-b)
+print("The product of the two numbers is:", a*b)
+print("The power of the two numbers is:", a**b)
+
+if b ==0:
+    print("\"Division by zero is not allowed\".")
+else:
+    print("The division of the two numbers is:", a/b)
+    print("The remainder of the two numbers is:", a%b)
+    print("The floor division of the two numbers is:", a//b)
+  
+print()
+print("Thank you so much for using this calculator")
+
